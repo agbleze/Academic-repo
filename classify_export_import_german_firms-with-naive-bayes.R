f7827bb121a1f5f8e9ea7b38c2c2e76c8a3b5c15
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-09-04 23:45:52 +0200

    Update classify_export_import_german_firms-with-naive-bayes.R

--- classify_export_import_german_firms-with-naive-bayes.R
+++ classify_export_import_german_firms-with-naive-bayes.R
@@ -3,3 +3,31 @@ Germany_2005_full_data_3_6_dta <- read_stata("~/Downloads/R FOR DATA ANALYSIS AN
 View(Germany_2005_full_data_3_6_dta)
 
 german_enterprise_data <- as.data.frame(Germany_2005_full_data_3_6_dta)
+str(german_enterprise_data)
+summary(german_enterprise_data)
+summary(german_enterprise_data$s7)
+str(german_enterprise_data$s7)
+table(factor(german_enterprise_data$s7))
+barplot(table(factor(german_enterprise_data$s7)))
+
+market_type <- factor(german_enterprise_data$s7, levels = c(2, 1))
+barplot(table(market_type))
+### using levels to reassign values to factors
+
+
+### working with factors
+levels(market_type)[1] <- "No_export"
+levels(market_type)[2] <- "export"
+
+barplot(table(market_type))
+
+
+
+
+gender <- c("male", "female", "none")
+gender <- factor(gender, levels = c("male", "female", "none"), ordered = TRUE)
+
+## converting to factors
+nums <- factor(c(1.2, 2.3, 3.4, 4.5))
+as.numeric(levels(nums)[nums])
+
